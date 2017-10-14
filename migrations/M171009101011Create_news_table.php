448a8f16171cd7a6027fb3f533380910ffc74a8f
Peter <xutongle@gmail.com>
2017-10-14 18:26:24 +0800

    init

--- migrations/M171009101011Create_news_table.php
+++ migrations/M171009101011Create_news_table.php
@@ -16,30 +16,21 @@ class M171009101011Create_news_table extends Migration
         }
 
         $this->createTable('{{%news}}', [
-            'id' => $this->primaryKey(),
-            'uuid' => $this->string(),
-            'category_id' => $this->string()->comment('栏目ID'),
-            'title' => $this->string()->notNull()->comment('标题'),
-            'sub_title'=>$this->string(80)->notNull()->comment('副标题'),
-            'description' => $this->string()->comment('描述'),
-            'status' => $this->boolean()->defaultValue(false)->comment('状态'),
-            'cover' => $this->string()->comment('封面'),
-            'comments' => $this->integer()->notNull()->defaultValue(0),
-            'supports' => $this->integer()->notNull()->defaultValue(0),
-            'collections' => $this->integer()->notNull()->defaultValue(0),
-            'views' => $this->integer()->notNull()->defaultValue(0),
-            'is_top' => $this->boolean()->notNull()->defaultValue(false)->comment('是否置顶'),
-            'is_best' => $this->boolean()->notNull()->defaultValue(false)->comment('是否精华'),
-            'user_id' => $this->integer()->notNull()->defaultValue(0),
-            'content' => $this->text()->notNull()->comment('内容'),
-            'created_at' => $this->integer()->notNull()->defaultValue(0),
-            'updated_at' => $this->integer()->notNull()->defaultValue(0),
-            'published_at' => $this->integer()->notNull()->defaultValue(0),
+            'id' => $this->primaryKey()->unsigned()->comment('ID'),
+            'user_id' => $this->integer()->unsigned()->notNull()->comment('User ID'),
+            'slug' => $this->string()->comment('Slug'),
+            'title' => $this->string()->notNull()->comment('Title'),
+            'sub_title'=>$this->string(80)->notNull()->comment('Sub Title'),
+            'description' => $this->string()->comment('Description'),
+            'status' => $this->boolean()->defaultValue(false)->comment('Status'),
+            'views' => $this->integer()->notNull()->defaultValue(0)->comment('Views'),
+            'url' => $this->string()->notNull()->comment('Url'),
+            'published_at' => $this->integer()->comment('Published At'),
+            'created_at' => $this->integer()->notNull()->comment('Created At'),
+            'updated_at' => $this->integer()->notNull()->comment('Updated At'),
         ], $tableOptions);
         $this->createIndex('{{%news_index_published_at}}', '{{%news}}', 'published_at');
-
-        $this->alterColumn('{{%news}}','uuid','varchar(50) BINARY');
-        $this->createIndex('{{%news_index_uuid}}', '{{%news}}', 'uuid');
+        $this->addForeignKey('{{%news_fk_1}}', '{{%news}}', 'user_id', '{{%user}}', 'id', 'CASCADE', 'RESTRICT');
     }
 
     public function down()
